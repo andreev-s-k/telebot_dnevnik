f6ce76c7ffe82cfb009edd135009a8404266b035
andreev-s-k <94913476+andreev-s-k@users.noreply.github.com>
2022-04-26 17:45:27 +0300

    Update main.py

--- main.py
+++ main.py
@@ -4,9 +4,7 @@ from telegram.ext import *
 from telegram import *
 from dnevnikru import Dnevnik
 from collections import Counter
-
-login = "andreyevsergey2005"
-password = "setinov5"
+from pprint import pprint
 
 logging.basicConfig(
     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.DEBUG
@@ -16,16 +14,15 @@ logger = logging.getLogger(__name__)
 
 TOKEN = '5225353944:AAGfXO0TWaKkrBJFakBbJ6LyHwFFOGyQeiY'
 
-dn = Dnevnik(login=login, password=password)
+dn = 0  # Dnevnik(login=login, password=password)
 
 reply_keyboard = [['Домашние задания на завтра'],
                   ['Расписание на неделю'],
                   ['Итоги недели'],
                   ['Оценки за четверть/семестр'],
-                  ['/help']]
+                  ['Аккаунт']]
 
 reply_keyboard2 = [['Оценки за неделю'], ['Изучаемые на недели темы'], ['Главное меню']]
-
 
 def start(update, context):
     markup = ReplyKeyboardMarkup(reply_keyboard, resize_keyboard=True)
@@ -35,21 +32,15 @@ def start(update, context):
         reply_markup=markup
     )
 
-
-def help(update, context):
-    update.message.reply_text(
-        "Я пока не умею помогать... Я только ваше эхо.")
-
-
 def homework(update, context):
     if not context.args:
         day = str(int(datetime.today().strftime('%d')) + 1) + datetime.today().strftime('.%m.%Y')
-        home_work = dn.homework(datefrom=day, days=0)
+        homeworks = dn.homework(datefrom=day, days=0)
     elif int(context.args[0]) > 0:
-        home_work = dn.homework(days=int(context.args[0]))
-    answer = 'Всего заданий: ' + str(home_work['homeworkCount']) + '\n'
-    for i in range(len(home_work['homework'])):
-        answer = answer + str(i + 1) + '.' + home_work['homework'][i][0] + ' | ' + home_work['homework'][i][1] + '\n'
+        homeworks = dn.homework(days=int(context.args[0]))
+    answer = 'Всего заданий: ' + str(homeworks['homeworkCount']) + '\n'
+    for i in range(len(homeworks['homework'])):
+        answer = answer + str(i + 1) + '.' + homeworks['homework'][i][0] + ' | ' + homeworks['homework'][i][1] + '\n'
     update.message.reply_text(
         answer
 
@@ -63,18 +54,24 @@ def echo(update, context):
         update.message.reply_text('Я владею следующей информацией об этом.\n'
                                   'Выберите интересующию Вас.',
                                   reply_markup=markup1)
-    if update.message.text == 'Оценки за неделю':
-        marks_week(update)
-    if update.message.text == 'Изучаемые на недели темы':
-        learn_week(update)
-    if update.message.text == 'Расписание на неделю':
-        timetable_week(update)
+    if dn != 0 or update.message.text == 'Аккаунт' or update.message.text == 'Главное меню' or update.message.text == 'Итоги недели':
+        if update.message.text == 'Оценки за неделю':
+            marks_week(update)
+        if update.message.text == 'Изучаемые на недели темы':
+            learn_week(update)
+        if update.message.text == 'Расписание на неделю':
+            timetable_week(update)
+        if update.message.text == 'Оценки за четверть/семестр':
+            marks(update, context)
+        if update.message.text == 'Домашние задания на завтра':
+            homework(update, context)
+    else:
+        update.message.reply_text('Данные аккаунта не указаны\n'
+                                  'Указать их можно в пункте "Аккаунт" главного меню.')
+    if 'Аккаунт' in update.message.text:
+        account(update, context)
     if update.message.text == 'Главное меню':
         start(update, context)
-    if update.message.text == 'Оценки за четверть/семестр':
-        marks(update, context)
-    if update.message.text == 'Домашние задания на завтра':
-        homework(update, context)
 
 
 def marks_week(update):
@@ -109,7 +106,7 @@ def timetable_week(update):
 
 def marks(update, context):
     answer = dn.marks()
-    print(answer)
+    pprint(answer)
     x = ''
     ans = 'Оценки за четверть/семестр: \n-----------------------'
     for i in answer:
@@ -124,6 +121,23 @@ def marks(update, context):
         x = ''
         y.clear()
     update.message.reply_text(ans)
+    ans = 'Средний балл: \n-----------------------'
+    for i in answer:
+        ans = ans + '\n' + i[1] + ' | ' + i[6]
+    update.message.reply_text(ans)
+
+
+def account(update, context):
+    global dn
+    print(context, context.args)
+    if context.args == None:
+        update.message.reply_text(
+            "Введите данные аккаунта\n"
+            "для этого введите команду:\n /account [логин] [пароль].\n")
+    else:
+        dn = Dnevnik(login=context.args[0], password=context.args[1])
+        update.message.reply_text(
+            "Данные аккаунта получены")
 
 
 def main():
@@ -131,12 +145,11 @@ def main():
     dp = updater.dispatcher
     dp.add_handler(CommandHandler("start", start))
     dp.add_handler(CommandHandler("homework", homework,
-                                  pass_args=True,
-                                  pass_job_queue=True,
-                                  pass_chat_data=True))
+                                  pass_args=True))
+    dp.add_handler(CommandHandler("account", account,
+                                  pass_args=True))
     echo_handler = MessageHandler(Filters.text & (~Filters.command), echo)
     dp.add_handler(echo_handler)
-    dp.add_handler(CommandHandler("help", help))
     updater.start_polling()
     updater.idle()
 
